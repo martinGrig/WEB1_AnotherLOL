dbf1430eeb0b8588bc123d65fd7a663742cfc4ff
Martin M. Grigorov <marty7566@gmail.com>
2019-03-16 22:35:26 +0100

    update

--- Submition B/Pages/JAVASCRIPT/main.js
+++ Submition B/Pages/JAVASCRIPT/main.js
@@ -23,3 +23,72 @@ function getInfo(){
   }
   console.log("incorrect  username  or password")
 }
+
+<script> 
+  function SignUp()                                    
+  { 
+      var name = document.forms["signup"]["name"];             
+      var email = document.forms["signup"]["emailadress"];    
+      var username = document.forms["signup"]["username"];  
+      var what =  document.forms["signup"]["message"];  
+      var password = document.forms["signup"]["password"];  
+      var repeatPassword = document.forms["signup"]["password"];
+    
+      if (name.value == "")                                  
+      { 
+        window.alert("Please enter your name."); 
+        name.focus(); 
+        return false; 
+      } 
+    
+      if (address.value == "")                               
+      { 
+          window.alert("Please enter your address."); 
+          name.focus(); 
+          return false; 
+      } 
+        
+      if (email.value == "")                                   
+      { 
+          window.alert("Please enter a valid e-mail address.");
+          email.focus(); 
+          return false; 
+      } 
+    
+      if (email.value.indexOf("@", 0) > 0)        
+      { 
+          window.alert("Please enter a valid e-mail address."); 
+          email.focus(); 
+          return false; 
+      } 
+    
+      if (email.value.indexOf(".", 0) > 0)                 
+      { 
+          window.alert("Please enter a valid e-mail address."); 
+          email.focus(); 
+          return false; 
+      } 
+    
+      if (phone.value == "")                           
+      { 
+          window.alert("Please enter your telephone number."); 
+          phone.focus(); 
+          return false; 
+      } 
+    
+      if (password.value == "")                        
+      { 
+          window.alert("Please enter your password"); 
+          password.focus(); 
+          return flase; 
+      } 
+    
+      if (what.selectedIndex > 1)                  
+      { 
+          alert("Please enter your course."); 
+          what.focus(); 
+          return false; 
+      } 
+    
+      return true; 
+}</script>
