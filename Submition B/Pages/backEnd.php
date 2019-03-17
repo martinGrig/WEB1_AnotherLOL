40011f3d0fc89d502bab4163a23559fe1744105e
Martin M. Grigorov <marty7566@gmail.com>
2019-03-17 21:36:46 +0100

    php

---
+++ Submition B/Pages/backEnd.php
@@ -0,0 +1,9 @@
+<?php
+    $name = $_POST["name"];
+    $email = $_POST["emailadress"];
+    $password = $_POST["password"];
+    $username = $_POST["username"];
+
+    echo $username;
+    echo 
+?>
